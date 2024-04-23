3b5177dbb7eb6105a8028d240892772a7a079ae5
SteliosNTI <stelios.pantelidis@elev.ga.ntig.se>
2024-04-23 13:47:51 +0200

    Skbidi

--- js.js
+++ js.js
@@ -1,5 +1,4 @@
-function click(filename) {
-    var filePath = "path/to/your/directory/" + filename;
-    window.open("https://www.youtube.com", "_blank");
+function klick(filename) {
+    window.open(filename, "_self");
 
 }
