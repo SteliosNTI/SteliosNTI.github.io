b84060d474b0c775e1f363b99443d222434a46bf
SteliosNTI <stelios.pantelidis@elev.ga.ntig.se>
2024-05-10 16:28:31 +0200

    m

--- js.js
+++ js.js
@@ -2,12 +2,11 @@ let oldBTN = null;
 
 function btn(BTNid) {
     let newBTN = document.getElementById(BTNid);
-    
-    // Check if this is the first button clicked or if it's a different button
+
     if (oldBTN && newBTN !== oldBTN) {
-        oldBTN.style.color = "white"; // Revert color of the previously clicked button
+        oldBTN.style.color = "white";
     }
 
-    newBTN.style.color = "red"; // Change color of the currently clicked button
-    oldBTN = newBTN; // Update the old button reference
+    newBTN.style.color = "red";
+    oldBTN = newBTN; 
 }
